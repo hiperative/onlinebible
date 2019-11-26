4e9abf91119334156877d2887d22b035eed8219e
Rafaell Lycan <sonny.webdsg@gmail.com>
2019-11-26 14:30:33 +0100

    Update release date to January 2020

--- src/components/containers/index.tsx
+++ src/components/containers/index.tsx
@@ -17,7 +17,7 @@ const IndexContainer = () => {
   return (
     <Container>
       <Logo src={logo} alt="La Biblia Online" />
-      <Pill>Disponible en Octubre 2019</Pill>
+      <Pill>Disponible en Enero 2020</Pill>
       <Title>Lea la Biblia Sagrada desde su ordenador, movil o tablet.</Title>
       <Text>
         Creemos que la Biblia es una historia unificada que nos lleva a Jesús y nuestra misión es
