cf72bb13ad91e96146070673c3b91ed7220efe7e
Rafaell Lycan <sonny.webdsg@gmail.com>
2024-06-25 14:44:39 +0200

    refactor: move content from old site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bibliaonline.es',
   // trailingSlash: "never",
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), tailwind(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    }
+  })],
   adapter: netlify(),
   output: "static",
   // prefetch: {
